17f3f36c72e1be34c0f4a9e74d09ce12d652e5a6
takato kokubo <takatokokubo@takatonoMacBook-Air.local>
2025-04-02 08:14:49 +0900

    viteのポート設定

--- src/resources/js/Pages/Mobile/MobileMain.tsx
+++ src/resources/js/Pages/Mobile/MobileMain.tsx
@@ -27,7 +27,7 @@ const MobileMain = () => {
     return (
         <div>
             <Header />
-            <h1>テストですね</h1>
+            <h1>テストですねカンナムスタイル</h1>
             <main className="flex-1 overflow-auto p-4">
                 {renderPage()}
             </main>
